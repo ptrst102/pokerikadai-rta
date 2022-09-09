6e17fab9eefe594cfb3963bd4b458ec6ab7c4da4
ptrst102 <ptrst102@gmail.com>
2022-09-09 18:21:29 +0900

    dashboardとgraphicsにVite+React導入

---
+++ dashboard/src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+const App: React.FC = () => {
+
+  return (
+    <div className="App">
+      <p>
+        Hello, I'm one of the panels in your bundle!
+        I'm where you put your controls.
+      </p>
+    </div>
+  )
+}
+
+export default App
---
+++ graphics/src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+const App: React.FC = () => {
+
+  return (
+    <div className="App">
+      <p>
+        Hello, I'm one of the graphics in your bundle!
+        I'm where you put your controls.
+      </p>
+    </div>
+  )
+}
+
+export default App
