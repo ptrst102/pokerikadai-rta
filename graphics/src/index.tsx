8b2fa35d2e5604d96912e925daace97b7cf9a488
ptrst102 <ptrst102@gmail.com>
2022-09-10 23:03:01 +0900

    dashboardとgraphicsのts-nodecg対応

--- graphics/src/index.tsx
+++ graphics/src/index.tsx
@@ -2,6 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import {
+  CreateNodecgInstance,
+  CreateNodecgConstructor
+} from 'ts-nodecg/browser';
+import { BundleConfig } from '../../nodecg/bundleConfig';
+import { ReplicantMap } from '../../nodecg/replicants';
+import { MessageMap } from '../../nodecg/messages';
+
+declare global {
+  interface Window {
+    nodecg: CreateNodecgInstance<
+      'pokerikadai-rta',
+      BundleConfig,
+      ReplicantMap,
+      MessageMap
+    >;
+    NodeCG: CreateNodecgConstructor<
+      'pokerikadai-rta',
+      BundleConfig,
+      ReplicantMap,
+      MessageMap
+    >;
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
